3f49f43d24925aad4c1219407c6f779489426712
ERDEM UÇAK <52521399+erdemucak@users.noreply.github.com>
2022-05-24 12:59:18 +0300

    Add files via upload

---
+++ hyperV_Replica_Status.ps1
@@ -0,0 +1,8 @@
+﻿$server = @("dtl1hyperv1","dtl1hyperv2","dtl1hyperv3","dtl1hyperv4","dtl1hyperv5","dtl1hyperv6","dtl1hyperv7","dtl1hyperv8","dtl1hyperv9") 
+ForEach($item in $server){
+Invoke-Command -ComputerName $server -ScriptBlock { Get-VMReplication | Select-Object VMName,State,Health,LastReplicationTime,ReplicaServer } | ConvertTo-Html -title "Hyper-V Replication Status"  | Out-File -FilePath C:\ScheduledTasks\HyperVReplications\report\replications.html }
+cd C:\ScheduledTasks\HyperVReplications\report
+$repcount= cat replications.html | Measure-Object | %{ $_.Count-10 }
+Send-MailMessage -Attachments "C:\ScheduledTasks\HyperVReplications\report\replications.html" -From replica@digiturk.com.tr -To techinfracorpbo@digiturk.com.tr -Body "Replikasyonu Yapilan Sunucu Sayisi: $repcount " -Subject "Hyper-V Replication Status" -SmtpServer 172.28.1.100
+Remove-Item -Path C:\ScheduledTasks\HyperVReplications\report\replications.html
+exit
